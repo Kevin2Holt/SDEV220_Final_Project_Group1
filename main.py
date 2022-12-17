ecb7cdaa2a0e35c75c6616237c86db1177034a9a
Kevin2Holt <kevin2holt@gmail.com>
2022-12-16 21:15:12 -0500

    Changed Trapizoid spelling. Added more generality with the keybinds (shouldn't be case-sensitive any more)

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ DIAMOND = "./imgs/Diamond.png"
 OVAL = "./imgs/Oval.png"
 RECT = "./imgs/Rect.png"
 RISING_RECT = "./imgs/Rising_Rect.png"
-TRAPIZOID = "./imgs/Trapizoid.png"
+TRAPEZOID = "./imgs/Trapizoid.png"
 WAVY_RECT = "./imgs/Wavy_Rect.png"
 #Easy-Change Settings
 TOOLS_ICON_PADDING = (2,5,5,5)#Left,Up,Right,Down
@@ -23,7 +23,7 @@ KEY_DIAMOND = 'W'
 KEY_OVAL = 'E'
 KEY_RECT = 'R'
 KEY_RISING_RECT = 'T'
-KEY_TRAPIZOID = 'Y'
+KEY_TRAPEZOID = 'Y'
 KEY_WAVY_RECT = 'U'
 
 
@@ -43,7 +43,7 @@ diamondIconImg = 	Image.open(DIAMOND)
 ovalIconImg = 		Image.open(OVAL)
 rectIconImg = 		Image.open(RECT)
 risingRectIconImg = Image.open(RISING_RECT)
-trapizoidIconImg = 	Image.open(TRAPIZOID)
+trapezoidIconImg = 	Image.open(TRAPEZOID)
 wavyRectIconImg = 	Image.open(WAVY_RECT)
 #Convert Image to PhotoImage (Readable by Label)
 #For Tool Bar
@@ -52,7 +52,7 @@ diamondIconLab = 	ImageTk.PhotoImage(diamondIconImg.resize((		2*TOOLS_BAR_SCALER
 ovalIconLab = 		ImageTk.PhotoImage(ovalIconImg.resize((			2*TOOLS_BAR_SCALER, 1*TOOLS_BAR_SCALER)))
 rectIconLab = 		ImageTk.PhotoImage(rectIconImg.resize((			2*TOOLS_BAR_SCALER, 1*TOOLS_BAR_SCALER)))
 risingRectIconLab = ImageTk.PhotoImage(risingRectIconImg.resize((	2*TOOLS_BAR_SCALER, 1*TOOLS_BAR_SCALER)))
-trapizoidIconLab = 	ImageTk.PhotoImage(trapizoidIconImg.resize((	2*TOOLS_BAR_SCALER, 1*TOOLS_BAR_SCALER)))
+trapezoidIconLab = 	ImageTk.PhotoImage(trapezoidIconImg.resize((	2*TOOLS_BAR_SCALER, 1*TOOLS_BAR_SCALER)))
 wavyRectIconLab = 	ImageTk.PhotoImage(wavyRectIconImg.resize((		2*TOOLS_BAR_SCALER, 1*TOOLS_BAR_SCALER)))
 #For Canvas
 circleLab = 	ImageTk.PhotoImage(circleIconImg.resize((		1*CANVAS_SCALER, 1*CANVAS_SCALER)))
@@ -60,7 +60,7 @@ diamondLab = 	ImageTk.PhotoImage(diamondIconImg.resize((		2*CANVAS_SCALER, 1*CAN
 ovalLab = 		ImageTk.PhotoImage(ovalIconImg.resize((			2*CANVAS_SCALER, 1*CANVAS_SCALER)))
 rectLab = 		ImageTk.PhotoImage(rectIconImg.resize((			2*CANVAS_SCALER, 1*CANVAS_SCALER)))
 risingRectLab = ImageTk.PhotoImage(risingRectIconImg.resize((	2*CANVAS_SCALER, 1*CANVAS_SCALER)))
-trapizoidLab = 	ImageTk.PhotoImage(trapizoidIconImg.resize((	2*CANVAS_SCALER, 1*CANVAS_SCALER)))
+trapezoidLab = 	ImageTk.PhotoImage(trapezoidIconImg.resize((	2*CANVAS_SCALER, 1*CANVAS_SCALER)))
 wavyRectLab = 	ImageTk.PhotoImage(wavyRectIconImg.resize((		2*CANVAS_SCALER, 1*CANVAS_SCALER)))
 
 
@@ -75,19 +75,19 @@ def update(flowchart=[]):
 		widget.destroy()
 	if(flowchart):
 		for i, item in enumerate(flowchart):
-			if(item.upper()==KEY_CIRCLE):
+			if(item.upper()==KEY_CIRCLE.upper()):
 				itemLabel = ttk.Label(canvasFrame, image=circleLab,		padding=CANVAS_ITEM_PADDING)
-			elif(item.upper()==KEY_DIAMOND):
+			elif(item.upper()==KEY_DIAMOND.upper()):
 				itemLabel = ttk.Label(canvasFrame, image=diamondLab,	padding=CANVAS_ITEM_PADDING)
-			elif(item.upper()==KEY_OVAL):
+			elif(item.upper()==KEY_OVAL.upper()):
 				itemLabel = ttk.Label(canvasFrame, image=ovalLab,		padding=CANVAS_ITEM_PADDING)
-			elif(item.upper()==KEY_RECT):
+			elif(item.upper()==KEY_RECT.upper()):
 				itemLabel = ttk.Label(canvasFrame, image=rectLab,		padding=CANVAS_ITEM_PADDING)
-			elif(item.upper()==KEY_RISING_RECT):
+			elif(item.upper()==KEY_RISING_RECT.upper()):
 				itemLabel = ttk.Label(canvasFrame, image=risingRectLab,	padding=CANVAS_ITEM_PADDING)
-			elif(item.upper()==KEY_TRAPIZOID):
-				itemLabel = ttk.Label(canvasFrame, image=trapizoidLab,	padding=CANVAS_ITEM_PADDING)
-			elif(item.upper()==KEY_WAVY_RECT):
+			elif(item.upper()==KEY_TRAPEZOID.upper()):
+				itemLabel = ttk.Label(canvasFrame, image=trapezoidLab,	padding=CANVAS_ITEM_PADDING)
+			elif(item.upper()==KEY_WAVY_RECT.upper()):
 				itemLabel = ttk.Label(canvasFrame, image=wavyRectLab,	padding=CANVAS_ITEM_PADDING)
 			itemLabel.grid(column=0,row=i)
 
@@ -118,12 +118,13 @@ def updateAdv(flowchart=[]):
 				itemLabel = ttk.Label(canvasFrame, image=rectIconLab,		padding=CANVAS_ITEM_PADDING)
 			elif(item[0]==KEY_RISING_RECT):
 				itemLabel = ttk.Label(canvasFrame, image=risingRectIconLab,	padding=CANVAS_ITEM_PADDING)
-			elif(item[0]==KEY_TRAPIZOID):
-				itemLabel = ttk.Label(canvasFrame, image=trapizoidIconLab,	padding=CANVAS_ITEM_PADDING)
+			elif(item[0]==KEY_TRAPEZOID):
+				itemLabel = ttk.Label(canvasFrame, image=trapezoidIconLab,	padding=CANVAS_ITEM_PADDING)
 			elif(item[0]==KEY_WAVY_RECT):
 				itemLabel = ttk.Label(canvasFrame, image=wavyRectIconLab,	padding=CANVAS_ITEM_PADDING)
 
 			itemLabel.grid(column=itemSet[1],row=itemSet[2])
+
 
 # Return contents from "Flowchartdata.txt" file into a list.
 def Read():
@@ -193,8 +194,8 @@ def symbol_rising_rect(event):
 
 
 def symbol_trapezoid(event):
-    Write(KEY_TRAPIZOID)
-    create_symbol(KEY_TRAPIZOID)
+    Write(KEY_TRAPEZOID)
+    create_symbol(KEY_TRAPEZOID)
 
 
 def symbol_wavy_rect(event):
@@ -208,13 +209,13 @@ def symbol_remove(event):
 
 
 """Bindings"""
-root.bind("<KeyPress-q>", symbol_circle)
-root.bind("<KeyPress-w>", symbol_diamond)
-root.bind("<KeyPress-e>", symbol_oval)
-root.bind("<KeyPress-r>", symbol_rect)
-root.bind("<KeyPress-t>", symbol_rising_rect)
-root.bind("<KeyPress-y>", symbol_trapezoid)
-root.bind("<KeyPress-u>", symbol_wavy_rect)
+root.bind("<KeyPress-"+KEY_CIRCLE.lower()+">", symbol_circle)
+root.bind("<KeyPress-"+KEY_DIAMOND.lower()+">", symbol_diamond)
+root.bind("<KeyPress-"+KEY_OVAL.lower()+">", symbol_oval)
+root.bind("<KeyPress-"+KEY_RECT.lower()+">", symbol_rect)
+root.bind("<KeyPress-"+KEY_RISING_RECT.lower()+">", symbol_rising_rect)
+root.bind("<KeyPress-"+KEY_TRAPEZOID.lower()+">", symbol_trapezoid)
+root.bind("<KeyPress-"+KEY_WAVY_RECT.lower()+">", symbol_wavy_rect)
 root.bind("<Return>", symbol_remove)
 
 """Read data on startup"""
@@ -237,7 +238,7 @@ diamondIcon = 		ttk.Label(toolsFrame, image=diamondIconLab,		padding=TOOLS_ICON_
 ovalIcon = 			ttk.Label(toolsFrame, image=ovalIconLab,		padding=TOOLS_ICON_PADDING)
 rectIcon = 			ttk.Label(toolsFrame, image=rectIconLab,		padding=TOOLS_ICON_PADDING)
 risingRectIcon = 	ttk.Label(toolsFrame, image=risingRectIconLab,	padding=TOOLS_ICON_PADDING)
-trapizoidIcon =		ttk.Label(toolsFrame, image=trapizoidIconLab,	padding=TOOLS_ICON_PADDING)
+trapezoidIcon =		ttk.Label(toolsFrame, image=trapezoidIconLab,	padding=TOOLS_ICON_PADDING)
 wavyRectIcon = 		ttk.Label(toolsFrame, image=wavyRectIconLab,	padding=TOOLS_ICON_PADDING)
 #Place Label on grid
 circleIcon.grid(	column=0, row=0)
@@ -245,7 +246,7 @@ diamondIcon.grid(	column=0, row=1)
 ovalIcon.grid(		column=0, row=2)
 rectIcon.grid(		column=0, row=3)
 risingRectIcon.grid(column=0, row=4)
-trapizoidIcon.grid(	column=0, row=5)
+trapezoidIcon.grid(	column=0, row=5)
 wavyRectIcon.grid(	column=0, row=6)
 
 """Tool Bar - Key Labels"""
@@ -255,7 +256,7 @@ diamondLabel = 		ttk.Label(toolsFrame, text="W")
 ovalLabel = 		ttk.Label(toolsFrame, text="E")
 rectLabel = 		ttk.Label(toolsFrame, text="R")
 risingRectLabel = 	ttk.Label(toolsFrame, text="T")
-trapizoidLabel = 	ttk.Label(toolsFrame, text="Y")
+trapezoidLabel = 	ttk.Label(toolsFrame, text="Y")
 wavyRectLabel = 	ttk.Label(toolsFrame, text="U")
 #Place Labels on grid
 circleLabel.grid(		column=0,row=0)
@@ -263,7 +264,7 @@ diamondLabel.grid(		column=0,row=1)
 ovalLabel.grid(			column=0,row=2)
 rectLabel.grid(			column=0,row=3)
 risingRectLabel.grid(	column=0,row=4)
-trapizoidLabel.grid(	column=0,row=5)
+trapezoidLabel.grid(	column=0,row=5)
 wavyRectLabel.grid(		column=0,row=6)
 
 
